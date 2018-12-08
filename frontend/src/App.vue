ba2fc6d0cc6f55cb788d8c57d75c3025791e8168
Gabriel L Martinez <g.lorenzo.martinez@gmail.com>
2018-12-08 11:22:00 -0400

    Create frontend application using vue js

---
+++ frontend/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app">
+    <HelloWorld title="Welcome to my Application"/>
+    <table id="my-table">
+      <tr>
+        <th>Titulo</th>
+        <th>Precio</th>
+        <th>Color</th>
+      </tr>
+      <Product v-for="product in products" :key="product.id" :title="product.title" :price="product.price" :color="product.color"/>
+    </table>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Product from './components/Product.vue'
+import axios from 'axios'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    Product
+  },
+  data() {
+    return {
+      products: []
+    }
+  },
+  mounted () {
+    axios.get('http://localhost:3000/').then(response => (this.products = response.data.products))
+  }
+}
+</script>
+
+<style>
+
+#my-table {
+  margin: 0 auto;
+}
+
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ frontend/src/components/Product.vue
@@ -0,0 +1,26 @@
+<template>
+    <tr>
+        <td>{{ title }}</td>
+        <td>${{ price }}</td>
+        <td>{{ color }}</td>
+    </tr>
+</template>
+
+<script lang="ts">
+
+export default {
+    name: 'Product',
+    props: {
+        title: String,
+        price: Number,
+        color: String
+    }
+}
+</script>
+
+<style>
+    td {
+        padding: 5px;
+    }
+</style>
+
